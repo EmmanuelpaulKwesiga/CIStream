bdc190d95bbbde3d725ae2984ecaf3e8e64bd2f9
Hunter <kwetsigae16@gmail.com>
2026-06-22 15:35:19 +0200

    Initial Commit

---
+++ CIStream/CIStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  CIStreamApp.swift
+//  CIStream
+//
+//  Created by Emmanuel Kwesiga on 22/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct CIStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
